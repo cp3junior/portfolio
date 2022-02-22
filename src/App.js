b9e4c5738b3fc6b9a5e9bb8fd048777e2a564ada
RAILALA Andriatsimarivo <r3andrew@gmail.com>
2022-02-22 14:24:14 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, createContext } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import "./Styles/Dark.scss";
+import "./Styles/Custom.scss";
+
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+import Home from "./Screens/Home";
+import Projects from "./Screens/Projects";
+import Project from "./Screens/Project";
+import NotFound from "./Screens/NotFound";
+
+import translation from "./Documents/translation.json";
+
+export const AppContext = createContext();
+
+const App = () => {
+  const [isDark, setIsDark] = useState(false);
+  const [language, setLanguage] = useState("en");
+
+  useEffect(() => {
+    const thm = localStorage.getItem("theme");
+    if (thm === "dark") setIsDark(true);
+
+    const lg = localStorage.getItem("language");
+    if (!lg) localStorage.setItem("language", "en");
+    else setLanguage(lg);
+  }, []);
+
+  const toggleTheme = () => {
+    setIsDark((prev) => {
+      if (prev) localStorage.setItem("theme", "light");
+      else localStorage.setItem("theme", "dark");
+      return !prev;
+    });
+  };
+
+  const changeLanguage = (key) => {
+    setLanguage(key);
+    localStorage.setItem("language", key);
+  };
+
+  const t = (text, key = "") => {
+    if (language === "en") return text;
+    return translation[key] ? translation[key] : "Pas de Traduction";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider value={{ language, t, changeLanguage, toggleTheme }}>
+      <div className={isDark ? "dark-theme" : ""}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="project/:projectId" element={<Project />} />
+            <Route path="404" element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </div>
+    </AppContext.Provider>
   );
-}
+};
 
 export default App;
