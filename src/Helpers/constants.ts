acbeeaff35576938445490bbfbf1be2019871bf4
RAILALA Andriatsimarivo <r3andrew@gmail.com>
2022-02-22 15:14:27 +0300

    add typescript

---
+++ src/Helpers/constants.ts
@@ -0,0 +1,6 @@
+import { Page } from "./interfaces";
+
+export const pages: Page[] = [
+  { name: "Home", path: "/", key: "header.home" },
+  { name: "Projects", path: "/projects", key: "header.projects" },
+];
